a1236e313dfa073b8fa1ab1ddb992521ab0683ab
George Mapaya <mambostudioz@gmail.com>
2020-12-10 01:39:33 -0600

    Add dark theme mobile styles & theme toggling functionality

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 /* *****variables***** */
+const themeToggle = document.getElementById("theme-toggle");
+const theme = document.querySelector("#theme-link");
 
 /* *****functions***** */
 
 /* *****event listeners***** */
+// Toggle between dark mode & light mode
+themeToggle.addEventListener("click", () => {
+  if (theme.getAttribute("href") === "./css/light-theme.css") {
+    // change theme toggle icon to sun
+    themeToggle.className = "fas fa-sun theme-toggle";
+    // switch to dark mode
+    theme.setAttribute("href", "./css/dark-theme.css");
+  } else {
+    // change theme toggle icon to moon
+    themeToggle.className = "fas fa-moon theme-toggle";
+    // switch to light mode
+    theme.setAttribute("href", "./css/light-theme.css");
+  }
+});
